dd4f45f80b728c2861a5da1acbc300335f3b27a3
karnav pargi <karnavkumar@gmail.com>
2018-04-16 09:22:16 +0530

    Regitration API Adeed

--- app/models/admin.server.model.js
+++ app/models/admin.server.model.js
@@ -1,21 +1,31 @@
 var mongoose = require('mongoose');
 var schema = mongoose.Schema;
-
+var bcrypt = require('bcrypt');
 var AdminSchema = new schema({
-    firstname:{type:String , required:true},
-    lastname:{type:String , required:true},
-    contactno:{type:Number , required:true},
-    password:{type:String , required:true},
-    emailId:{type:String , required:true},
-    profilePic:{type:String , required:true},
-    username:{type:String, required:true}
+    firstname: { type: String, required: true },
+    lastname: { type: String, required: true },
+    contactno: { type: Number, required: true },
+    password: { type: String, required: true },
+    emailId: { type: String, required: true, unique: true },
+    profilePic: { type: String, },
+    username: { type: String, required: true }
 }, {
-    collection: 'admins',
-    timestamps:true
-});
+        collection: 'admins',
+        timestamps: true
+    });
+
+AdminSchema.methods.generateHash = function (password) {
+    console.log(password, '----------------------------------------------------------------------------')
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(9));
+}
+
+AdminSchema.methods.validPassword = function (password, userSchemaPassword) {
+    console.log(password, userSchemaPassword);
+    return bcrypt.compareSync(password, userSchemaPassword);
+};
 
 var adminModel = mongoose.model('admin', AdminSchema);
 
 module.exports = {
-    Admin : adminModel
+    Admin: adminModel
 }
